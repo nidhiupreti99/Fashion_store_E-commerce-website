98dcd690ae8d04f3ee27dfc9188778ee0ff1ae4e
nidhiupreti99 <nidhiupreti23@gmail.com>
2022-01-23 22:27:56 +0530

    user orders model implemented

---
+++ E-CommerceFasionBackend/src/main/java/ECommerce/DAO/OrderDAOImpl.java
@@ -0,0 +1,44 @@
+package ECommerce.DAO;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import ECommerce.model.OrderDetail;
+
+@Repository("orderDAO")
+@Transactional
+public class OrderDAOImpl implements OrderDAO{
+
+	@Autowired
+	SessionFactory sessionFactory;
+	
+	
+	@Override
+	public boolean saveOrder(OrderDetail orderDetail) {
+		try {
+			sessionFactory.getCurrentSession().save(orderDetail);
+			return true;
+		}catch(Exception e){
+		    return false;
+		}
+	}
+
+	@Override
+	public boolean updateCart(String username) {
+		Session session = sessionFactory.getCurrentSession();
+		Query query = session.createQuery("update CartItem set paymentStatus='P' where username=:uname");
+		query.setParameter("uname", username);
+		int row_eff=query.executeUpdate();
+		
+		if(row_eff>0)
+			return true;
+		else
+			return false;
+	}
+
+	
+}
